12ed79672459d9af4d66b04de83fa44c63fb4992
Bogdan Stoica <bogdan.stoica@students.plymouth.ac.uk>
2018-02-15 19:09:24 +0000

    Minor Documentation changes
    Added Mind Map for project, Highlight 2 and started small experimentation with Unity Classes for the research phase.

---
+++ B and F Hate Mazes/Assets/Scripts/CreateCell.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateCell : MonoBehaviour {
+
+	//Cell Class
+    public class Cell
+    {
+        private int cell_Row;
+        private int cell_Column;
+        private Cell neighbour_North;
+        private Cell neighbour_South;
+        private Cell neighbour_East;
+        private Cell neighbour_West;
+
+        public Cell()
+        {
+            cell_Row = 1;
+            cell_Column = 1;
+        }
+
+    }
+    
+
+    
+    
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
